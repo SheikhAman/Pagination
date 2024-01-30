0d84a577c95d6b4426da54c3861d8ed36e9d35bf
SheikhAman <amansk664@gmail.com>
2024-01-30 10:09:25 +0600

    Added GetX Infinity Paginate done.

--- api_paginate_example/lib/getx_pagination/getx_pagenate_view.dart
+++ api_paginate_example/lib/getx_pagination/getx_pagenate_view.dart
@@ -8,6 +8,7 @@ class GetXPaginateView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(PaginateController());
+    controller.buildMethod();
       return Scaffold(
       appBar: AppBar(
         title: const Text('GetX Paginate with API'),
@@ -15,12 +16,19 @@ class GetXPaginateView extends StatelessWidget {
       body: RefreshIndicator(
         onRefresh: controller.refresh,
         child: Obx(
-        () =>  ListView.builder(
+        () => controller.isLoading.value ?
+       const  Center(
+         child: CircularProgressIndicator(),
+       )  :
+        ListView.builder(
             controller: controller.scrollController,
             padding: const EdgeInsets.all(8),
             itemCount: controller.items.length + 1,
             itemBuilder: (context, index) {
               if (index < controller.items.length) {
+                // index < items.length -> (1.2.3..23.24.25 = index) < (items.length = 25+1)
+                // 25 tar jonno ListTile hobe.
+                // 26 number item hole circularProgressIndicator or No data msg show korbe hasMore variable er upor nirvor kore.
                 var item = controller.items[index];
                 return ListTile(
                   title: Text(item),
--- api_paginate_example/lib/getx_pagination/paginate_controller.dart
+++ api_paginate_example/lib/getx_pagination/paginate_controller.dart
@@ -1,35 +1,40 @@
-
-
 import 'dart:io';
-
 import 'package:api_paginate_example/getx_pagination/remote_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+
 class PaginateController extends GetxController{
 
   RxBool isLoading = false.obs;
-  RxBool hasMore = true.obs;
+  bool isFirstCall = true;
+  RxBool hasMore = true.obs; // From start we hope it is max than 25 so that we will load all data in the start.
   RxList items = [].obs;
 
   final scrollController = ScrollController();
   int limit = 25;
   int page = 1;
 
+  // I want isLoading will only be true when it gets called first time after that it will always be false when it gets called how can i do it.
+
   void getOrderList() async {
  //   bool isOnline = await InternetChecker.hasInternet();
   //  if (isOnline) {
       try {
-        isLoading.value = true;
-        await RemoteServices().getOrderListData(limit: limit,page: page).then((response) async {
+        // isLoading.value = true;
+        isLoading.value = isFirstCall;
+        await RemoteServices().getOrderListData(limit: limit, page: page)
+            .then((response) async {
        //   orderData.clear();
        //   orderData.value = response;
        final List newItems = response;
           page++;
-
+          // length starts from 1.
+          // (newItems.length < limit) than 25 then hasMore value will be false.
           if (newItems.length < limit) {
             hasMore.value = false;
           }
+          // Map function will return iterable and we will convert it to List using toList fun.
           items.addAll(newItems.map<String>((item) {
             final number = item['id'];
             return 'Item $number';
@@ -42,25 +47,28 @@ class PaginateController extends GetxController{
         debugPrint('getOrderList HttpException: $error');
       } finally {
         isLoading.value = false;
-
+        isFirstCall = false; // set isFirstCall to false after the first call
       }
+
   //  }
+
   }
 
 
   Future refresh() async {
-
+    // all values are reInitialize & Api fun gets called.
       isLoading.value = false;
+      bool isFirstCall = true;
       hasMore.value = true;
       page = 1;
       items.clear();
-
       getOrderList();
   }
 
   @override
   void onInit() {
-    getOrderList();
+
+    // getOrderList();
     scrollController.addListener(() {
       if (scrollController.position.maxScrollExtent == scrollController.offset) {
         getOrderList();
@@ -70,6 +78,16 @@ class PaginateController extends GetxController{
     // TODO: implement onInit
     super.onInit();
   }
+
+  buildMethod(){
+    getOrderList();
+    // scrollController.addListener(() {
+    //   if (scrollController.position.maxScrollExtent == scrollController.offset) {
+    //     getOrderList();
+    //   }
+    // });
+  }
+
 
 
 }
